954ce0c299f52e77281bedf2fb1d8a3e3f870964
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-21 22:54:37 +0400

    feat: added hard data

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+const movies = [
+  {id: '1', name: 'The Shawshank Redemption', genre: 'Drama'},
+  {id: '2', name: 'The Green Mile', genre: 'Criminal'},
+  {id: '3', name: 'The Lord of the Rings: The Return of the King', genre: 'fantasy'},
+  {id: '4', name: 'Interstellar', genre: 'Science-fiction'},
+]
+
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
