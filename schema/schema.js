763067e1953916c932fd81ef437bad478401a254
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-21 23:11:01 +0400

    feat: added type GraphQLID for id

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,17 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 const movies = [
   { id: "1", name: "The Shawshank Redemption", genre: "Drama" },
   { id: "2", name: "The Green Mile", genre: "Criminal" },
-  { id: "3", name: "The Lord of the Rings: The Return of the King", genre: "fantasy" },
-  { id: "4", name: "Interstellar", genre: "Science-fiction" },
+  { id: 3, name: "The Lord of the Rings: The Return of the King", genre: "fantasy" },
+  { id: 4, name: "Interstellar", genre: "Science-fiction" },
 ];
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -22,9 +22,9 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find(movie => movie.id === args.id);
+        return movies.find(movie => movie.id == args.id);
       },
     },
   },
