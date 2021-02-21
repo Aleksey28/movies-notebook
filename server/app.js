6e7c84f99b7a14617c48130116f0bee7becc1a22
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-22 02:50:12 +0400

    connected to mongoDB

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
+
+const app = express();
+const PORT = 3000;
+
+mongoose.connect(
+  'mongodb+srv://Aleksey:12345qwert@graphql-tutorial.ccvvp.mongodb.net/graphql-tutorial?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  },
+);
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
+
+app.listen(PORT, (err) => (err ? console.log(err) : console.log(`Server started on port - ${PORT}`)));
