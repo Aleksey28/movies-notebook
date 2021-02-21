b8a5980d42258b10f5b108f93f3eaaf1122f80e1
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-21 23:07:46 +0400

    feat: described resolve Query

--- schema/schema.js
+++ schema/schema.js
@@ -2,11 +2,11 @@ const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const movies = [
-  {id: '1', name: 'The Shawshank Redemption', genre: 'Drama'},
-  {id: '2', name: 'The Green Mile', genre: 'Criminal'},
-  {id: '3', name: 'The Lord of the Rings: The Return of the King', genre: 'fantasy'},
-  {id: '4', name: 'Interstellar', genre: 'Science-fiction'},
-]
+  { id: "1", name: "The Shawshank Redemption", genre: "Drama" },
+  { id: "2", name: "The Green Mile", genre: "Criminal" },
+  { id: "3", name: "The Lord of the Rings: The Return of the King", genre: "fantasy" },
+  { id: "4", name: "Interstellar", genre: "Science-fiction" },
+];
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
@@ -24,7 +24,7 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-
+        return movies.find(movie => movie.id === args.id);
       },
     },
   },
