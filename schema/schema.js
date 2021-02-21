a97de2206b439397cddcf67a6c9ad6b8b0c6a045
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-21 23:14:34 +0400

    feat: added directors

--- schema/schema.js
+++ schema/schema.js
@@ -2,10 +2,16 @@ const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 const movies = [
-  { id: "1", name: "The Shawshank Redemption", genre: "Drama" },
-  { id: "2", name: "The Green Mile", genre: "Criminal" },
+  { id: 1, name: "The Shawshank Redemption", genre: "Drama" },
+  { id: 2, name: "The Green Mile", genre: "Criminal" },
   { id: 3, name: "The Lord of the Rings: The Return of the King", genre: "fantasy" },
   { id: 4, name: "Interstellar", genre: "Science-fiction" },
+];
+
+const directors = [
+  { id: 1, name: "Frank Darabont", age: 62 },
+  { id: 2, name: "Peter Jackson", age: 59 },
+  { id: 3, name: "Christopher Nolan", age: 50 },
 ];
 
 const MovieType = new GraphQLObjectType({
