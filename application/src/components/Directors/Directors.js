52915ab5fe2bae590457b2f2c9482c0785dd29fb
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-22 18:26:05 +0400

    feat: added component Directors

---
+++ application/src/components/Directors/Directors.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+import DirectorsTable from '../DirectorsTable/DirectorsTable';
+import DirectorsForm from '../DirectorsForm/DirectorsForm';
+
+import withHocs from './DirectorsHoc';
+
+const Directors = ({ classes }) => {
+  const [open, setOpen] = useState(false);
+  const [stateData, setStateData] = useState({
+    id: null,
+    name: '',
+    age: 0,
+  });
+
+  const handleClickOpen = (data) => {
+    setOpen(true);
+    setStateData({
+      ...data,
+    });
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setStateData({
+      name: '',
+      age: 0,
+      id: null,
+    });
+  };
+
+  const handleChange = (name) => ({ target }) => {
+    setStateData((prev) => ({
+      [name]: target.value,
+      ...prev,
+    }));
+  };
+
+  const { name, age, id } = stateData;
+  return (
+    <>
+      <DirectorsForm
+        handleChange={handleChange}
+        selectedValue={{ name, age, id }}
+        open={open}
+        onClose={handleClose}
+      />
+      <div className={classes.wrapper}>
+        <DirectorsTable onOpen={handleClickOpen} onClose={handleClose} />
+        <Fab onClick={() => handleClickOpen(null)} color="primary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </div>
+    </>
+  );
+};
+
+export default withHocs(Directors);
