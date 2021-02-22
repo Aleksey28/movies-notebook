930484207e722e83842fa41841056726830da150
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-22 18:57:32 +0400

    feat: changed component App and index

--- application/src/App.js
+++ application/src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './components/theme';
+import Tabs from './components/Tabs/Tabs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <Tabs />
+    </MuiThemeProvider>
   );
 }
 
