462e01a374201852d4e6c6ddf9b48b2b5346952d
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-22 21:17:18 +0400

    feat: connected apollo client

--- application/src/App.js
+++ application/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import theme from './components/theme';
 import Tabs from './components/Tabs/Tabs';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Tabs />
-    </MuiThemeProvider>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <Tabs />
+      </MuiThemeProvider>
+    </ApolloProvider>
   );
 }
 
